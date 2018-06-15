5e2e984cca815b593ee127594b785caad497d7af
Timo Jaaskelainen <timojaask@gmail.com>
2018-06-15 12:39:17 +0100

    Move Google Analytics ID to environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,12 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+  console.log('Requested .env file')
+}
+
+console.log(JSON.stringify(process.env))
+
+console.log('TRACKING ID: ' + process.env.GA_TRACKING_ID)
+
 module.exports = {
   siteMetadata: {
     title: `helloalta`
@@ -6,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-118464744-1",
+        trackingId: process.env.GA_TRACKING_ID,
         // Puts tracking script in the head instead of the body
         head: false,
         // Setting this parameter is optional
